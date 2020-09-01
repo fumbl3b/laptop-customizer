025aedf4fc3e4969aa1fd163914639d786d4d0bc
Harrison Winkler <harryewinkler@thinkful.com>
2020-08-31 22:40:55 -0400

    finished except last component

--- src/MainSummary.js
+++ src/MainSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Cart from './Cart';
+import SummaryTotal from './SummaryTotal';
 
 export default function MainSummary(props) {
     const total = Object.keys(props.selected).reduce(
@@ -15,12 +16,10 @@ export default function MainSummary(props) {
                 selected={props.selected}
                 currencyFormat={props.currencyFormat}
             />
-            <div className="summary__total">
-                <div className="summary__total__label">Total</div>
-                <div className="summary__total__value">
-                    {props.currencyFormat.format(total)}
-                </div>
-            </div>
+            <SummaryTotal 
+                currencyFormat={props.currencyFormat}
+                total={total}
+            />
         </section>
     );
 }
